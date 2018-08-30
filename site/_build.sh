8dbb493b28570ebea7d31aedd88899a36fb09bc8
vdmeer <sven@vandermeer.de>
2018-08-30 19:16:09 +0100

    added research library

--- site/_build.sh
+++ site/_build.sh
@@ -43,30 +43,30 @@ sed -i.bak 's/x2013; <\/title>/x2013; Supervision<\/title>/g' target/site/teachi
 rm target/site/teaching-supervision.html.bak
 
 
-
-
-
-sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Supervision<\/li>/g' target/site/research-notes-policy.html
+sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Research Notes - Policy-based Management<\/li>/g' target/site/research-notes-policy.html
 rm target/site/research-notes-policy.html.bak
 sed -i.bak 's/x2013; <\/title>/x2013; Research Notes - Policy-based Management<\/title>/g' target/site/research-notes-policy.html
 rm target/site/research-notes-policy.html.bak
 
-sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Supervision<\/li>/g' target/site/research-notes-cybernetics.html
+sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Research Notes - Cybernetics<\/li>/g' target/site/research-notes-cybernetics.html
 rm target/site/research-notes-cybernetics.html.bak
 sed -i.bak 's/x2013; <\/title>/x2013; Research Notes - Cybernetics<\/title>/g' target/site/research-notes-cybernetics.html
 rm target/site/research-notes-cybernetics.html.bak
 
-sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Supervision<\/li>/g' target/site/research-notes-ina.html
+sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Research Notes - INA<\/li>/g' target/site/research-notes-ina.html
 rm target/site/research-notes-ina.html.bak
 sed -i.bak 's/x2013; <\/title>/x2013; Research Notes - INA<\/title>/g' target/site/research-notes-ina.html
 rm target/site/research-notes-ina.html.bak
 
-sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Supervision<\/li>/g' target/site/research-notes-internet-history.html
+sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Research Notes - Internet History<\/li>/g' target/site/research-notes-internet-history.html
 rm target/site/research-notes-internet-history.html.bak
 sed -i.bak 's/x2013; <\/title>/x2013; Research Notes - Internet History<\/title>/g' target/site/research-notes-internet-history.html
 rm target/site/research-notes-internet-history.html.bak
 
-
+sed -i.bak 's/<li class="active "><\/li>/<li class="active ">Research Library<\/li>/g' target/site/research-library.html
+rm target/site/research-library.html.bak
+sed -i.bak 's/x2013; <\/title>/x2013; Research Library<\/title>/g' target/site/research-library.html
+rm target/site/research-library.html.bak
 
 
 

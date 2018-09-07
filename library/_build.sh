0e213a4b5dc53298ece7f79d0334bfe2ee536627
vdmeer <sven@vandermeer.de>
2018-09-07 09:28:18 +0100

    library: refactoring for multi-site use

--- library/_build.sh
+++ library/_build.sh
@@ -12,7 +12,7 @@ for file in `ls *.adoc`
 do
     echo "  -> ${file%.*}"
     lib_home=`pwd`
-    asciidoctor $file -a toc=left $attributes -a vdmsite-lib-home=$lib_home
+    asciidoctor $file -a toc=left $attributes -a  skb-library-home=$lib_home
     chmod 644 ${file%.*}.html
 done
 
@@ -22,9 +22,10 @@ if [ "$1" == "-l" ]; then
     (rm -f ../../../../library/*.html)
     cp *.html ../../../../library
 else
-    echo "  publish to vdmeer.github.io repo"
-    rm -fr ../../vdmeer.github.io/library/*
-    cp *.html ../../vdmeer.github.io/library/
+    echo "  publish to skb/docs"
+    rm -fr ../docs/library
+    mkdir ../docs/library
+    cp *.html ../docs/library
     #rsync -a --prune-empty-dirs --include '*/' --include '*.html' --exclude '*' . ../../vdmeer.github.io/library/
 fi
 

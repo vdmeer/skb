2c6007b7b4afce15816272dfb61c2085541fab64
vdmeer <sven@vandermeer.de>
2018-10-09 23:50:20 +0100

    fw: bild change

--- src/framework/build.sh
+++ src/framework/build.sh
@@ -7,8 +7,11 @@ mkdir -p src/main/bash/doc/manual
 mkdir -p src/main/bash/man/man1
 mkdir -p src/main/bash/cache
 
-TOOL_DIR=`(cd build/libs;pwd)`
-export SF_SKB_FW_TOOL=$TOOL_DIR/skb-framework-tool-0.0.0-all.jar
+mkdir -p src/main/bash/bin/java
+cp build/libs/skb-framework-tool-0.0.0-all.jar src/main/bash/bin/java
+
+#TOOL_DIR=`(cd build/libs;pwd)`
+#export SF_SKB_FW_TOOL=$TOOL_DIR/skb-framework-tool-0.0.0-all.jar
 src/main/bash/bin/skb-framework -c
 src/main/bash/bin/skb-framework -M dev -r dev/build-inst
 src/main/bash/bin/skb-framework -h
